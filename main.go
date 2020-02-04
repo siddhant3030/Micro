5898c324063bafc743dbf85feb0a7f786e63cf75
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-04 22:17:49 +0530

    no new variables on left side of :=

--- main.go
+++ main.go
@@ -7,4 +7,9 @@ import (
 func main() {
 	power := 9000
 	fmt.Printf("It's over %d\n", power)
+
+	// COMPILER ERROR:
+	// no new variables on left side of :=
+	power := 9001
+	fmt.Printf("It's also over %d\n", power)
 }
