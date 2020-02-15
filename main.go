a4b9b67032eb6f0ddb944a35dbd4d90a9120888e
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-15 22:17:21 +0530

    mux errors

--- main.go
+++ main.go
@@ -15,8 +15,8 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", helloWorld).Methods("GET")
-	myRouter.HandleFunc("/", AllPeoples).Methods("GET")
-	myRouter.HandleFunc("/user/{name}/{age}", NewPeople).Methods("POST")
+	myRouter.HandleFunc("/", AllUsers).Methods("GET")
+	myRouter.HandleFunc("/user/{name}/{age}", NewUser).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
 
---
+++ user.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var db *gorm.DB
+var err error
+
+type User struct {
+	gorm.Model
+	Name string
+	Email string
+}
+
+func InitialMigration() {
+	db, err = gorm.Open("postgres", "dbname=users sslmode=disable")
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Failed to connect to database")
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&User{})
+}
+
+
+func AllUsers(w http.ResponseWriter, r *http.Request) {
+	db, err = gorm.Open("postgres", "users")
+	if err != nil {
+		panic("Could not connect to database")
+	}
+	defer db.Close()
+
+	var users []User
+	db.Find(&users)
+	json.NewEncoder(w).Encode(users)
+}
+
+func NewUser(w http.ResponseWriter, r *http.Request) {
+	db, err = gorm.Open("postgres", "users")
+	if err != nil {
+		panic("Could not connect to database")
+	}
+	defer db.Close()
+
+	vars := mux.Vars(r)
+	name := vars["name"]
+	email := vars["email"]
+
+	db.Create(&User{Name: name, Email: email})
+	fmt.Fprintf(w, "new user successfully created")
+}
