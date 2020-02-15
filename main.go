1b9f59e6abbbd67fdaaa054fb0ca1fe1a250a21f
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-16 01:25:11 +0530

    new try

--- main.go
+++ main.go
@@ -1,28 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/gorilla/mux"
-)
-
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
-}
-
-func handleRequests() {
-	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/", helloWorld).Methods("GET")
-	myRouter.HandleFunc("/", AllUsers).Methods("GET")
-	myRouter.HandleFunc("/user/{name}/{age}", NewUser).Methods("POST")
-	log.Fatal(http.ListenAndServe(":8081", myRouter))
-}
-
 func main() {
-
-	fmt.Println("GO orm tut")
-	InitialMigration()
-	handleRequests()
+	fmt.println("GO postgres tut")
 }
