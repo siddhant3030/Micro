2359974b4527e86d35e38b8e5c555602670b4f32
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-16 02:19:08 +0530

    ok

--- src/modules/profile/repositry/repositry.go
+++ src/modules/profile/repositry/repositry.go
@@ -1,5 +1,13 @@
 package repositry
 
 import (
-	"https://github.com/siddhant3030/KubernetesApi/blob/master/src/modules/profile"
+	"https://github.com/siddhant3030/KubernetesApi/tree/master/src/modules/profile/module"
 )
+
+type ProfileRepository interface {
+	Save(*model.Profile) error
+	Update(string, *model.Profile) error
+	Delete(string) error
+	FindByID(string) (*model.Profile, error)
+	FindAll() (model.Profiles, error)
+  }
