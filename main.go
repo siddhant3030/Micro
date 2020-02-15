757d88f0678f6109fc04e040e138ae750fb47731
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-16 02:43:30 +0530

    bad choice

--- main.go
+++ main.go
@@ -1,5 +1,49 @@
 package main
 
+import (
+	"github.com/siddhant3030/KubernetesApi/config"
+	"github.com/siddhant3030/KubernetesApi/src/modules/profile/module"
+	"github.com/siddhant3030/KubernetesApi/src/modules/profile/repositry"
+)
+
 func main() {
-	fmt.println("Go postgres")
+	fmt.Println("Go Postgres Tutorial")
+
+	db, err := config.GetPostgersDB()
+  
+	if err != nil {
+	  fmt.Println(err)
+	}
+  
+  
+	wury := model.NewProfile()
+	wury.ID = "01"
+	wury.Name = "Rob"
+	wury.Email = "rob@yahoo.com"
+	fmt.Println(wury)
+  
+	profileRepositoryPostgres := repository.NewProfileRepositoryPostgres(db)
+  
+	profiles, err := getProfiles(profileRepositoryPostgres)
+  
+	if err != nil {
+	  fmt.Println(err)
+	}
+  
+	fmt.Println("=======================")
+  
+	for _, v := range profiles {
+	  fmt.Println(v)
+	}
+  }
+  
+  func saveProfile(p *model.Profile, repo repository.ProfileRepository) error{
+	err := repo.Save(p)
+  
+	if err != nil {
+	  return err
+	}
+  
+	return nil
+  }
 }
--- src/modules/profile/repositry/repositry.go
+++ src/modules/profile/repositry/repositry.go
@@ -1,13 +1,9 @@
 package repositry
 
-import (
-	"https://github.com/siddhant3030/KubernetesApi/tree/master/src/modules/profile/module"
-)
-
 type ProfileRepository interface {
 	Save(*model.Profile) error
 	Update(string, *model.Profile) error
 	Delete(string) error
 	FindByID(string) (*model.Profile, error)
 	FindAll() (model.Profiles, error)
-  }
+}
