c42b8a45b53b11c2c1a7838a49938c2d3eb15cfb
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-16 19:04:00 +0530

    api with postgres and test cases

--- New/src/modules/profile/repositry/repositry.go
+++
@@ -1,9 +0,0 @@
-package repositry
-
-type ProfileRepository interface {
-	Save(*model.Profile) error
-	Update(string, *model.Profile) error
-	Delete(string) error
-	FindByID(string) (*model.Profile, error)
-	FindAll() (model.Profiles, error)
-}
