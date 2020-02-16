5b551a8bb129f56a64bc59492ba3efc995a789bc
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-16 19:22:07 +0530

    imported pq

---
+++ app.go
@@ -0,0 +1,14 @@
+import (
+	"database/sql"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+type App struct {
+	Router *mux.Router
+	DB     *sql.DB
+}
+
+func (a *App) Initialize(user, password, dbname string) {}
+
