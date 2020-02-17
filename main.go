6b58a197ca0be53acf69bda6fc50e04903490397
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-17 14:22:07 +0530

    bug

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
-import "os"
+import "net/http"
 
 func main() {
-	a := App{}
-	a.Initialize(
-		os.Getenv("APP_DB_USERNAME"),
-		os.Getenv("APP_DB_PASSWORD"),
-		os.Getenv("APP_DB_NAME"))
 
-	a.Run(":8080")
+	http.ListenAndServe(":8080", nil)
+	
 }
