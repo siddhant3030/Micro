0a41e48a70a3cb8df6e6a624fe05deafcd348fb8
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-18 22:56:04 +0530

    few changes

--- main.go
+++ main.go
@@ -1,85 +1,29 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
 
-	"./models"
-	"github.com/astaxie/beego/orm"
-	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-var ORM orm.Ormer
-
-func init() {
-	models.ConnectToDb()
-	ORM = models.GetOrmObject()
-}
-
 func main() {
-	// Creates a gin router with default middleware:
-	// logger and recovery (crash-free) middleware
-	router := gin.Default()
 
-	router.POST("/createUser", createUser)
-	router.GET("/readUsers", readUsers)
-	router.PUT("/updateUser", updateUser)
-	router.DELETE("/deleteUser", deleteUser)
+	db, err := gorm.Open("postgres", "user=postgres password=postgres dbname=gorm sslmode=disable")
 
-	// By default it serves on :8080 unless a
-	// PORT environment variable was defined.
-	router.Run(":3000")
-	// router.Run(":3000") for a hard coded port
-}
-
-func createUser(c *gin.Context) {
-	var newUser models.Users
-	c.BindJSON(&newUser)
-	_, err := ORM.Insert(&newUser)
-	if err == nil {
-		c.JSON(http.StatusOK, gin.H{
-			"status":    http.StatusOK,
-			"email":     newUser.Email,
-			"user_name": newUser.UserName,
-			"user_id":   newUser.UserId})
-	} else {
-		c.JSON(http.StatusInternalServerError,
-			gin.H{"status": http.StatusInternalServerError, "error": "Failed to create the user"})
+	if err != nil {
+		panic(err.Error())
 	}
-}
 
-func readUsers(c *gin.Context) {
-	var user []models.Users
-	_, err := ORM.QueryTable("users").All(&user)
-	if err == nil {
-		c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "users": &user})
-	} else {
-		c.JSON(http.StatusInternalServerError,
-			gin.H{"status": http.StatusInternalServerError, "error": "Failed to read the users"})
-	}
-}
+	defer db.Close()
 
-func updateUser(c *gin.Context) {
-	var updateUser models.Users
-	c.BindJSON(&updateUser)
-	_, err := ORM.QueryTable("users").Filter("email", updateUser.Email).Update(
-		orm.Params{"user_name": updateUser.UserName,
-			"password": updateUser.Password})
-	if err == nil {
-		c.JSON(http.StatusOK, gin.H{"status": http.StatusOK})
-	} else {
-		c.JSON(http.StatusInternalServerError,
-			gin.H{"status": http.StatusInternalServerError, "error": "Failed to update the users"})
-	}
-}
+	database := db.DB()
 
-func deleteUser(c *gin.Context) {
-	var delUser models.Users
-	c.BindJSON(&delUser)
-	_, err := ORM.QueryTable("users").Filter("email", delUser.Email).Delete()
-	if err == nil {
-		c.JSON(http.StatusOK, gin.H{"status": http.StatusOK})
-	} else {
-		c.JSON(http.StatusInternalServerError,
-			gin.H{"status": http.StatusInternalServerError, "error": "Failed to delete the users"})
+	err = database.Ping()
+
+	if err != nil {
+		panic(err.Error())
 	}
+
+	fmt.Println("Connection was successfull")
 }
