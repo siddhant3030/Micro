fff1eaad71ee092129ec6878f9efc7c87318af07
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-19 21:42:45 +0530

    init function get triggered

--- gorm/lib/gorm/redis_pool.ex
+++ gorm/lib/gorm/redis_pool.ex
@@ -9,4 +9,19 @@ defmodule Gorm.RediPool do
   def start_link() do
     GenServer.start_link(__MODULE__, [])
   end
+
+  def init([]) do
+    pool_opts = [
+      name: {:local, :redix_poolboy},
+      worker_module: Redix,
+      size: 10,
+      max_overflow: 5,
+    ]
+
+    children = [
+      :poolboy.child_spec(:redix_poolboy, pool_opts, @redis_connection_params)
+    ]
+
+    supervise(children, strategy: :one_for_one, name: __MODULE__)
+  end
 end
