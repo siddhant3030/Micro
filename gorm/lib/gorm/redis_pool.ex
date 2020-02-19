38a3a1f977f69607de94ec6e3dfa02960d19ffd1
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-19 21:45:19 +0530

    command and pipeline added

--- gorm/lib/gorm/redis_pool.ex
+++ gorm/lib/gorm/redis_pool.ex
@@ -24,4 +24,12 @@ defmodule Gorm.RediPool do
 
     supervise(children, strategy: :one_for_one, name: __MODULE__)
   end
+
+  def command(command) do
+    :poolboy.transaction(:redix_poolboy, &Redix.command(&1, command))
+  end
+
+  def pipeline(commands) do
+    :poolboy.transaction(:redix_poolboy, &Redix.pipeline(&1, commands))
+  end
 end
