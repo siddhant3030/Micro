a185caa465dada08c42375387c65e2c054ad88e1
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-19 20:34:52 +0530

    added the dependencies for redix and poolboy

--- gorm/mix.exs
+++ gorm/mix.exs
@@ -42,7 +42,9 @@ defmodule Gorm.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:poolboy, "~> 1.5"},
+      {:redix, ">= 0.0.0"}
     ]
   end
 
