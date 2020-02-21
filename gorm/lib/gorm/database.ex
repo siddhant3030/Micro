60e377f2a0d9e74c348940a3d1e3c68cf80e7563
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-21 13:55:03 +0530

    callback function for set and get

--- gorm/lib/gorm/database.ex
+++ gorm/lib/gorm/database.ex
@@ -3,18 +3,26 @@ defmodule Gorm.Database do
   alias Gorm.Accounts
   alias Gorm.Accounts.User
   alias Gorm.Repo
+  alias Gorm.RedisPool
 
   def start_link(args) do
     GenServer.start_link(__MODULE__, [])
   end
 
-  def list(pid) do
-    GenServer.call(pid, :list)
-  end
-
-
   def init(list) do
     {:ok, []}
+  end
+
+  def get(pid, key) do
+    GenServer.call(pid, {:get, key})
+  end
+
+  def set(pid, key, value) do
+    Genserver.cast(pid, {:set, key, value})
+  end
+
+  def list(pid) do
+    GenServer.call(pid, :list)
   end
 
   def handle_call(:list, _from, state) do
@@ -22,6 +30,17 @@ defmodule Gorm.Database do
 
     {:reply, my_models, state}
   end
+
+  def handle_call({:get, key}, _from, storage_pid) do
+    val = case RedisPool.retrieve(storage_pid, key) do
+      {:ok, value} -> value
+      nil -> nil
+    end
+    {:reply, val, storage_pid}
+  end
+
+  def handle_cast({:set, key, value}, storage_pid) do
+    {:ok, _key} = RedisPool.insert(storage_pid, key, value)
 end
 
 
@@ -36,34 +55,3 @@ end
 
 
 
-# def list(pid) do
-  #   GenServer.call(pid, :list)
-  # end
-
-  # def get(pid, key) do
-  #   GenServer.call(pid, {:get, key})
-  # end
-
-  # def set(pid, key, value) do
-  #   GenServer.cast(pid, {:set, key, value})
-  # end
-
-  # def handle_call(:list, _from, state) do
-  #   my_models = Accounts.list_users()
-
-  #   {:reply, my_models, state}
-  # end
-
-  # def handle_call({:get, key}, _from, storage_pid) do
-  #   val = case RedisPool.retrieve(storage_pid, key) do
-  #     {:ok, value} -> value
-  #     nil -> nil
-  #   end
-  #   {:reply, val, storage_pid}
-  # end
-
-  # def handle_cast({:set, key, value}, storage_pid) do
-  #   {:ok, _key} = RedisPool.insert(storage_pid, key, value)
-
-  #   {:noreply, storage_pid}
-  # end
