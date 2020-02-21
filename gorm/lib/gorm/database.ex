949efc09ec2f53f27db733e15e53b59a7a7bb6b2
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-21 18:44:13 +0530

    process

--- gorm/lib/gorm/database.ex
+++ gorm/lib/gorm/database.ex
@@ -1,15 +1,13 @@
 defmodule Gorm.Database do
   use GenServer
   alias Gorm.Accounts
-  alias Gorm.Accounts.User
-  alias Gorm.Repo
   alias Gorm.RedisPool
 
-  def start_link(args) do
+  def start_link(_args) do
     GenServer.start_link(__MODULE__, [])
   end
 
-  def init(list) do
+  def init(_list) do
     {:ok, []}
   end
 
@@ -17,13 +15,13 @@ defmodule Gorm.Database do
     GenServer.call(pid, {:userone, id})
   end
 
-  # def get(pid, key) do
-  #   GenServer.call(pid, {:get, key})
-  # end
+  def get(pid, key) do
+    GenServer.call(pid, {:get, key})
+  end
 
-  # def set(pid, key, value) do
-  #   GenServer.cast(pid, {:set, key, value})
-  # end
+  def set(pid, key, value) do
+    GenServer.cast(pid, {:set, key, value})
+  end
 
   def list(pid) do
     GenServer.call(pid, :list)
@@ -38,6 +36,11 @@ defmodule Gorm.Database do
   def handle_call({:userone, id}, _from, state) do
     users = Accounts.get_user!(id)
     {:reply, users, state}
+  end
+
+  def handle_cast({:set, key, value}, pid) do
+  {:ok, _key} = RedisPool.insert(pid, key, value)
+  {:noreply, pid}
   end
 end
 
