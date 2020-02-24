3315803dedb4cee572dad67beeeffe4fc4d868c3
siddhant3030 <ssiddhant3030@gmail.com>
2020-02-24 07:28:00 +0530

    without genserver implementation

--- gorm/lib/gorm/redis_pool.ex
+++ gorm/lib/gorm/redis_pool.ex
@@ -1,3 +1,13 @@
 defmodule Gorm.RedisPool do
+  alias Gorm.Accounts
 
+  def start_link(url), do: Redix.start_link(url)
+
+  def check(conn), do: Redix.command!(conn, ["PING"])
+
+  def get(conn, key), do: Redix.command!(conn, ["GET", key])
+
+  def set(conn, key, value), do: Redix.command!(conn, ["SET", key, value])
+
+  def user(id), do: Accounts.get_user!(id)
 end
